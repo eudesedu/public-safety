ef63805df716b80ca5439fc6d9aef0b2b04aef98
eudesedu <eudes.edu@gmail.com>
2020-03-29 22:59:06 -0300

    update the main, load-socrata, and load-csv function.

--- all-scripts/mod-extract.py
+++ all-scripts/mod-extract.py
@@ -7,33 +7,71 @@ import sys
 
 def f_load_crimes_full_socrata(url, token, email, password, tag):
     """
-    Non-public datasets require a token authentication to extract the full records of information.
+    Non-public datasets require a token authentication to extract a sample of records.
     """
     d_crime_socrata = Socrata(url, token, email, password).get(tag, limit=50000)
     d_crime_full = pd.DataFrame.from_records(d_crime_socrata)
     return d_crime_full
 
+def f_load_crimes_full_csv(setwd):
+    """
+    Chicago crimes dataset from 2001 to present for offline use in other applications.
+    """
+    os.chdir(setwd)
+    variable_list = ['ID', 
+                     'Date', 
+                     'Block', 
+                     'IUCR', 
+                     'Primary Type', 
+                     'Description', 
+                     'Location Description', 
+                     'Arrest', 
+                     'Domestic', 
+                     'Beat', 
+                     'District', 
+                     'Community Area', 
+                     'Year', 
+                     'Updated On', 
+                     'Latitude', 
+                     'Longitude']
+    d_crime_csv = pd.read_csv("Crimes_-_2001_to_present.csv", chunksize=100000,  usecols=variable_list) 
+    chunk_variable_list = []  
+    for chunk in d_crime_csv:
+        chunk_variable_list.append(chunk)
+    return chunk_variable_list
+
 def f_main():
     """
-    Automated execution to generate the environment configuration.
+    Automated execution to generate the environment configuration and parser argument.
     """
     descr = """
-        Define the parser argument.
+        Define the parser argument to load a dataset via Socrata.
     """
     parser = argparse.ArgumentParser(description=descr)
     parser.add_argument('-load_dataset_socrata', 
                         dest='load_dataset_socrata', 
                         action='store_const', 
                         const=True, 
-                        help='Call the environment function')
+                        help='Call the f_load_crimes_full_socrata function')
+    parser.add_argument('-load_dataset_csv', 
+                        dest='load_dataset_csv', 
+                        action='store_const', 
+                        const=True, 
+                        help='Call the f_load_crimes_full_csv function')
     cmd_args = parser.parse_args()
+
+    # The environment configuration is a dictionary of path directory, URLs, email, passwords, 
+    # and access tokens to set function arguments.
     importlib.import_module('mod-environment')
+
     if cmd_args.load_dataset_socrata: 
         f_load_crimes_full_socrata(os.environ.get('API_URL'), 
                                    os.environ.get('API_TOKEN'), 
                                    os.environ.get('API_EMAIL'), 
                                    os.environ.get('API_PASSWORD'),
                                    os.environ.get('API_TAG'))
+    if cmd_args.load_dataset_csv: 
+        f_load_crimes_full_csv(os.environ.get('DIR_DATASET'))
 
 if __name__ == '__main__':
     f_main()

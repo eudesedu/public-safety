e4f151373f52cbf3a0964e5bc8c09a0dd90eb5e8
eudesedu <eudes.edu@gmail.com>
2020-04-10 19:58:35 -0300

    review load csv function.

--- all-scripts/mod-extract.py
+++ all-scripts/mod-extract.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sodapy import Socrata
+import numpy as np
 import importlib
 import argparse
 import os
@@ -21,25 +22,19 @@ def f_load_crimes_full_csv(setwd):
     os.chdir(setwd)
     variable_list = ['ID', 
                      'Date', 
-                     'Block', 
-                     'IUCR', 
+                     'Block',  
                      'Primary Type', 
-                     'Description', 
-                     'Location Description', 
-                     'Arrest', 
-                     'Domestic', 
-                     'Beat', 
-                     'District', 
+                     'Description',  
+                     'Arrest',
                      'Community Area', 
-                     'Year', 
-                     'Updated On', 
                      'Latitude', 
                      'Longitude']
-    d_crime_csv = pd.read_csv("Crimes_-_2001_to_present.csv", chunksize=100000,  usecols=variable_list) 
-    chunk_variable_list = []  
-    for chunk in d_crime_csv:
-        chunk_variable_list.append(chunk)
-    return chunk_variable_list
+    d_crime_csv = pd.read_csv('Crimes_-_2001_to_present.csv',
+                              usecols=variable_list).astype({'ID': np.uint32,
+                                                             'Community Area': 'float16',
+                                                             'Latitude': 'float16',
+                                                             'Longitude': 'float16'})
+    return d_crime_csv
 
 def f_main():
     """
